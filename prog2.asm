29f39f856be08874a06b935bfc25fdb15ffe6731
Nirmal Tamang <lamanirmal27@gmail.com>
2023-08-29 20:22:16 +0545

    8080

---
+++ prog2.asm
@@ -0,0 +1,19 @@
+  start:LXI H, C000H
+        MVI C, 0AH
+        MVI D, 00H
+   here:MOV A, M
+        INX H
+        CMP M
+        JC go 
+        JZ go
+        MOV B, M ;swaping here
+        MOV M, A
+        DCX H
+        MOV M, B
+        MVI D, 01H
+     go:DCR C
+     	JNZ here
+        MOV A, D
+        CPI 01H
+        JZ start
+        hlt
